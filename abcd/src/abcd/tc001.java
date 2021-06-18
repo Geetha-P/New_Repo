18b6cf3a5feabfb7b7bd2e1582d52595d15b8880
priya <priya@prasad>
2021-06-18 23:24:55 +0530

    moving to github ,main repo

---
+++ abcd/src/abcd/tc001.java
@@ -0,0 +1,21 @@
+package abcd;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class tc001 {
+	@Test
+	public void testone()
+	{
+		
+		//System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		//ChromeDriver driver = new ChromeDriver();
+		//driver.("https://www.google.com");
+		//driver.findElementById("email").sendKeys("hello");
+		System.out.println("success");
+		//driver.quit();
+		
+	}
+	
+
+}
